b6d04a2c8cb2e2cceaae1bc1412cd02fc34b5c72
Michel Idriss <michelidriss41@gmail.com>
2026-07-13 12:34:35 +0000

    Introduce CryptixError base class for structured exception hierarchy

--- cryptix_engine/exceptions.py
+++ cryptix_engine/exceptions.py
@@ -1,13 +1,18 @@
-class AuthenticationError(Exception):
+class CryptixError(Exception):
+    """Base class for all Cryptix engine errors."""
+    pass
+
+
+class AuthenticationError(CryptixError):
     """Raised when authentication fails (wrong password or tampered file)."""
     pass
 
 
-class FormatError(Exception):
+class FormatError(CryptixError):
     """Raised when container format is invalid."""
     pass
 
 
-class VersionMismatchError(Exception):
+class VersionMismatchError(CryptixError):
     """Raised when file format version is unsupported."""
     pass
